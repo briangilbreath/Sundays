b2f07224511e85e1be847206883ef026acdc5fca
briangilbreath <bgilbreath87@gmail.com>
2013-05-05 14:23:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sundays::Application.config.secret_token = 'be91d049dfea386841117cade43bdb39e0856bcb3fa68ec793a79bcf13a0738ae5c828335ff82397ac6d614e77d63fad7ed40295cc70fda2a14913a4a1e17c82'
